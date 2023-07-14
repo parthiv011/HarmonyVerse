090e7d29af43dee6cc1c5cac3d3166377064953c
parthiv <parmarparthiv77@gmail.com>
2023-07-14 18:03:05 +0530

    updating more views

--- HarmonyVerse/app/views.py
+++ HarmonyVerse/app/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+def audio(request):
+    return render(request,'audio.html')
+def artist(request):
+    return render(request,'artist.html')
+def playlist(request):
+    return render(request,'playlist.html')
+def search(request):
+    return render(request,'searchartists.html')
+def album(request):
+    return render(request,'album.html')
+def about(request):
+    return render(request,'about.html')
